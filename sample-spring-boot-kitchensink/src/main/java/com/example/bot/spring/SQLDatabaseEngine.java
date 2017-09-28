5040ffa6e32d5d03d09c6bd8d1647873364ec6e2
user <user@mo220126ho25de-MacBook-Pro.local>
2017-09-28 18:15:32 +0800

    Test #1

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -11,14 +11,35 @@ import java.net.URI;
 public class SQLDatabaseEngine extends DatabaseEngine {
 	@Override
 	String search(String text) throws Exception {
-		//Write your code here
+		String result = null;
+		Connection connection = getConnection();
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement("SELECT response FROM linechatbot WHERE keyword = ?");
+			stmt.setString(1, text);
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			if(rs.next())
+			result = rs.getString(1);
+			
+			rs.close();
+			connection.close();
+			stmt.close();
+		} catch (Exception e) {
+			log.info(e.toString());
+		}
+		
+		if (result != null)
+			return result;
+		
 		return null;
 	}
 	
 	
 	private Connection getConnection() throws URISyntaxException, SQLException {
 		Connection connection;
-		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+		URI dbUri = new URI(System.getenv("postgres://rvrivnphsviggz:0c01d945bdadb8c751deacbca1824d4b7d727e2b04ddfe2aad18cb8d89e24360@ec2-50-19-218-160.compute-1.amazonaws.com:5432/daok0fndubs7ft"));
 
 		String username = dbUri.getUserInfo().split(":")[0];
 		String password = dbUri.getUserInfo().split(":")[1];
